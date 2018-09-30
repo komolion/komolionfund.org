a2155d685d75b2ccd90bdb5b5b6b5524d78e1e7c
Mason Everett <masonheverett@gmail.com>
2018-09-30 13:17:27 -0500

    More CI work

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,8 +1,8 @@
 git config user.name "$USER_NAME"
 git config user.email "$USER_EMAIL"
 
-git checkout "$DEPLOY_BRANCH"
-git pull origin "$DEPLOY_BRANCH"
+git checkout master
+git pull origin master
 
 find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
 mv _site/* .
@@ -10,6 +10,4 @@ rm -R _site/
 
 git add -fA
 git commit --allow-empty -m "$(git log develop -1 --pretty=%B)"
-git push origin "$DEPLOY_BRANCH"
-
-echo "deployed successfully"
+git push origin master
