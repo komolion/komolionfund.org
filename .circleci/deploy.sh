d800648a50f8482c8ea7007d3838c7909a05d4e1
Mason Everett <masonheverett@gmail.com>
2018-09-30 14:15:54 -0500

    Updating build process

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,13 +1,13 @@
 git config user.name "$USER_NAME"
 git config user.email "$USER_EMAIL"
 
-git checkout master
-git pull origin master
+git checkout dist
+git reset --hard origin/master
 
 find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
 mv _site/* .
 rm -R _site/
 
 git add -fA
-git commit --allow-empty -m "$(git log origin/develop -1 --pretty=%B)"
-git push origin master
+git commit --allow-empty -m "$(git log origin/master -1 --pretty=%B)"
+git push origin dist
