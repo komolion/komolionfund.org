3d2a740fcd6a0430794ce998f7d24380648a6c81
Mason Everett <masonheverett@gmail.com>
2018-09-30 13:31:00 -0500

    Fixing commit to master on deploy

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -9,5 +9,5 @@ mv _site/* .
 rm -R _site/
 
 git add -fA
-git commit --allow-empty -m "$(git log develop -1 --pretty=%B)"
+git commit --allow-empty -m "$(git log origin/develop -1 --pretty=%B)"
 git push origin master
