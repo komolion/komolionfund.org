76b691ea8944d17d479cd1b763847abb414804dc
Mason Everett <masonheverett@gmail.com>
2018-09-30 14:26:41 -0500

    Using master as dist branch

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,14 +1,14 @@
 git config user.name "$USER_NAME"
 git config user.email "$USER_EMAIL"
 
+git checkout develop
 git checkout master
-git checkout dist
-git reset --hard master
+git reset --hard develop
 
 find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
 mv _site/* .
 rm -R _site/
 
 git add -fA
-git commit --allow-empty -m "$(git log master -1 --pretty=%B)"
-git push origin dist
+git commit --allow-empty -m "$(git log develop -1 --pretty=%B)"
+git push origin master
